ad3d574b6477852a96305a8d9670da21de5ffb92
Dario Piotrowicz <dario@cloudflare.com>
2023-10-12 11:48:43 +0100

    update prerelease

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {}
 module.exports = nextConfig
 
 if(process.env.NODE_ENV === 'development') {
-    const { setupDevBindings } = require('@cloudflare/next-on-pages/dev-bindings');
+    const { setupDevBindings } = require('@cloudflare/next-on-pages/next-dev');
 
     setupDevBindings({
         r2Buckets: ['MY_R2'],
